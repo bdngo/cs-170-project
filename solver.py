7c27141c27825fd6b96145f5274ea29923e67518
Bryan Ngo <bryanngo@berkeley.edu>
2021-05-03 21:36:08 -0700

    add failed set intersection

--- solver.py
+++ solver.py
@@ -4,6 +4,8 @@ from utils import is_valid_solution, calculate_score
 import sys
 from os.path import basename, normpath
 import glob
+import time
+import random
 
 
 def solve(H):
@@ -16,7 +18,6 @@ def solve(H):
     """
     H = G.copy()
     c, k = [], []
-    # finding c and k
     num_nodes = H.number_of_nodes()
     if num_nodes <= 30:
         c_num, k_num = 1, 15
@@ -25,30 +26,61 @@ def solve(H):
     elif num_nodes <= 100:
         c_num, k_num = 5, 100
 
-    for _ in range(c_num):
-        curr_shortest = nx.shortest_path(H, 0, num_nodes - 1, weight="weight")
-        if len(curr_shortest) == 2:
-            break
-        find_sum_weights = lambda x: sum([H.edges[x, i]["weight"] for i in H.adj[x] if i != 0 and i != num_nodes - 1])
-        least_weight = min(curr_shortest[1:-1], key=find_sum_weights)
+    ctr = 0
+    while c_num > 0 and ctr < 10:
         H_cp = H.copy()
-        H_cp.remove_node(least_weight)
-        if not nx.is_connected(H_cp):
+        H_cp.remove_node(num_nodes-1)
+        shortest_dict = nx.shortest_path(H_cp, 0, weight="weight")
+        #shortest_adj_paths = [shortest_dict[i] for i in list(H.adj[num_nodes - 1])]
+        shortest_adj_paths = [shortest_dict[i] for i in list(H.adj[num_nodes - 1]) if shortest_dict.get(i) != None]
+        sorted_paths = sorted(shortest_adj_paths, key=lambda path: sum([H_cp[path[i]][path[i+1]]['weight'] for i in range(len(path)-1)]))
+        curr_remaining = [set(i[1:-1]) for i in sorted_paths[:-1] if len(i) > 2]
+        if curr_remaining == []:
             break
-        H.remove_node(least_weight)
-        c.append(least_weight)
+        found_node = set.intersection(*curr_remaining) - set(sorted_paths[-1])
+        if found_node != set():
+            chosen_node = random.choice(list(found_node))
+            H_cp = H.copy()
+            H_cp.remove_node(chosen_node)
+            if nx.is_connected(H_cp):
+                H.remove_node(chosen_node)
+                c.append(chosen_node)
+            ctr += 1
+            break
+        while found_node == set() and len(curr_remaining) > 1:
+            curr_remaining = curr_remaining[:-1]
+            found_node = set.intersection(*curr_remaining) - set(sorted_paths[-1])
+        if found_node == set():
+            break
+        chosen_node = random.choice(list(found_node))
+        H_cp = H.copy()
+        H_cp.remove_node(chosen_node)
+        if nx.is_connected(H_cp):
+            H.remove_node(chosen_node)
+            c.append(chosen_node)
+        else: 
+            ctr += 1
+        c_num -= 1
 
-    for _ in range(k_num):
-        curr_shortest = nx.shortest_path(H, 0, num_nodes - 1, weight="weight")
-        pg = nx.path_graph(curr_shortest)
-        min_edge_src, min_edge_dest = min(pg.edges, key=lambda x: H.edges[x[0], x[1]]["weight"])
-        H_cp = H.copy()
-        H_cp.remove_edge(min_edge_src, min_edge_dest)
-        if not nx.is_connected(H_cp):
-            break
-        H.remove_edge(min_edge_src, min_edge_dest)
-        k.append((min_edge_src, min_edge_dest))
+    H_cp = H.copy()
+    max_spanning_tree = nx.maximum_spanning_tree(H_cp, weight="weight")
+    H_cp.remove_edges_from(list(max_spanning_tree.edges))
+    k = sorted(list(H_cp.edges), key=lambda x: H_cp.edges[x[0], x[1]]["weight"])[:k_num]
+    H.remove_edges_from(k)
+    
+    # H_cp = H.copy()
+    # for u, v in H_cp.edges:
+    #     H.edges[u, v]["weight"] *= -1
+    # _, paths = nx.single_source_bellman_ford(H_cp, 0, weight="weight")
+    # max_paths_tree = nx.Graph()
+    # for path in paths.values():
+    #     nx.add_path(max_paths_tree, path, weight="weight")
+    # for u, v in max_paths_tree.edges:
+    #     max_paths_tree.edges[u, v]["weight"] *= -1
+    # H_cp.remove_edges_from(list(max_paths_tree.edges))
+    # k = sorted(list(H_cp.edges), key=lambda x: H_cp.edges[x[0], x[1]]["weight"])[:k_num]
     return c, k
+
 
 
 # Here's an example of how to run your solver.
@@ -66,14 +98,16 @@ def solve(H):
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
-    try:
-        inputs = glob.glob(f'inputs/{sys.argv[1]}/*')
-        for input_path in inputs:
-            output_path = f'outputs/{sys.argv[1]}/' + basename(normpath(input_path))[:-3] + '.out'
-            G = read_input_file(input_path)
-            c, k = solve(G)
-            assert is_valid_solution(G, c, k)
-            distance = calculate_score(G, c, k)
-            write_output_file(G, c, k, output_path)
-    except IndexError:
-        print("Empty size argument")
+    inputs = glob.glob(f'inputs/{sys.argv[1]}/*')
+    scores = []
+    start = time.time()
+    for input_path in inputs:
+        output_path = f'outputs/{sys.argv[1]}/' + basename(normpath(input_path))[:-3] + '.out'
+        G = read_input_file(input_path)
+        c, k = solve(G)
+        assert is_valid_solution(G, c, k)
+        distance = calculate_score(G, c, k)
+        write_output_file(G, c, k, output_path)
+        scores.append(distance)
+    end = time.time()
+    print(f"Elapsed: {end - start} s\nMaximum score: {max(scores)}\nMinimum score: {min(scores)}\nAverage score: {sum(scores) / len(scores)}")
